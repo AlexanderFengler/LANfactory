0ea2bad9ab170430554426dd02ef1c81392f79f5
Alexander Fengler <alexanderfengler@gmx.de>
2021-08-02 18:41:06 -0400

    typo

--- lanfactory/trainers/keras_mlp.py
+++ lanfactory/trainers/keras_mlp.py
@@ -40,7 +40,7 @@ class DataGenerator(keras.utils.Sequence):
 
     def __len__(self):
         'Denotes the number of batches per epoch'
-        return int(np.floor((len(self.file_IDs) * self.file_shape_dict['data'][0]) / self.batch_size))
+        return int(np.floor((len(self.file_IDs) * self.file_shape_dict['inputs'][0]) / self.batch_size))
 
     def __getitem__(self, index):
         'Generate one batch of data'
