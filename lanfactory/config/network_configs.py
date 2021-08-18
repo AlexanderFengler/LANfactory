70af836fbe7623be4ce6879d876d3218fed11de0
Alexander Fengler <alexanderfengler@gmx.de>
2021-08-18 19:00:39 -0400

    temporarily not load tensorflow

--- lanfactory/config/network_configs.py
+++ lanfactory/config/network_configs.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-import keras 
+#import tensorflow as tf
+#import keras 
 
 network_config_mlp = {'layer_types': ['dense', 'dense', 'dense'],
                       'layer_sizes': [100, 100, 1],
@@ -12,19 +12,5 @@ train_config_mlp = {'batch_size': 128,
                     'optimizer': 'adam',
                     'learning_rate': 0.002,
                     'loss': 'huber',
-                    'metrics': [tf.keras.losses.MeanSquaredError(name = 'MSE'), tf.keras.losses.Huber(name = 'Huber')],
+                    'metrics': None, #'metrics': [tf.keras.losses.MeanSquaredError(name = 'MSE'), tf.keras.losses.Huber(name = 'Huber')],
                     'callbacks': ['checkpoint', 'earlystopping', 'reducelr']}
-
-
-                            # config['mlp_hyperparameters'] = {'hidden_layers': [100, 100, 120, 1],
-                            #      'hidden_activations': ["tanh", "tanh", "tanh", "linear"],
-                            #      'filters': [128, 128, 128, 128],
-                            #      'batch_size': 100000,
-                            #      'n_epochs': 100, # CHANGE AGAINs
-                            #      'learning_rate': .002, # I think was originally 0.0002
-                            #      'momentum': .7,
-                            #      'model_type': "dnnregressor",
-                            #      'optimizer': "adam",
-                            #      'log': True,
-                            #      'loss': "huber",
-                            #      'gpu_x7': '2'}
