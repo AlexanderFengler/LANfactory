6c52cda365aeafee5d48b34f2dc64e2991613ffb
Alexander Fengler <alexanderfengler@gmx.de>
2022-11-17 21:55:28 -0500

    drop keras remains

--- lanfactory/config/network_configs.py
+++ lanfactory/config/network_configs.py
@@ -1,7 +1,6 @@
 network_config_mlp = {'layer_sizes': [100, 100, 1],
                       'activations': ['tanh', 'tanh', 'linear'],
                       'train_output_type': 'logprob'}
-                      # 'callbacks': ['checkpoint', 'earlystopping', 'reducelr']}
 
 train_config_mlp = {'cpu_batch_size': 128,
                     'gpu_batch_size': 256,
@@ -11,5 +10,3 @@ train_config_mlp = {'cpu_batch_size': 128,
                     'weight_decay': 0.,
                     'loss': 'huber',
                     'save_history': True}
-                    # 'metrics': [tf.keras.losses.MeanSquaredError(name = 'MSE'), tf.keras.losses.Huber(name = 'Huber')],
-                    # 'callbacks': ['checkpoint', 'earlystopping', 'reducelr']}
