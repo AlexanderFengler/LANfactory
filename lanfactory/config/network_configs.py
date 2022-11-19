62c6b35ccf719f04fd4d9a18cbef27f0cac0b308
Alexander Fengler <alexanderfengler@gmx.de>
2022-11-18 22:20:01 -0500

    additions to config examples

--- lanfactory/config/network_configs.py
+++ lanfactory/config/network_configs.py
@@ -4,9 +4,11 @@ network_config_mlp = {'layer_sizes': [100, 100, 1],
 
 train_config_mlp = {'cpu_batch_size': 128,
                     'gpu_batch_size': 256,
-                    'n_epochs': 10,
+                    'n_epochs': 5,
                     'optimizer': 'adam',
                     'learning_rate': 0.002,
+                    'lr_scheduler': 'reduce_on_plateau',
+                    'lr_scheduler_params': {},
                     'weight_decay': 0.,
                     'loss': 'huber',
                     'save_history': True}
