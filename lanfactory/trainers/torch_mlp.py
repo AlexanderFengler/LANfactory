ce6ef63f4f4eae7fcd1348e301bac7a86d47ca79
Alexander Fengler <alexanderfengler@gmx.de>
2023-02-02 22:23:50 -0500

    drop print

--- lanfactory/trainers/torch_mlp.py
+++ lanfactory/trainers/torch_mlp.py
@@ -102,10 +102,8 @@ class DatasetTorch(torch.utils.data.Dataset):
 
         if self.label_prelog_cutoff_low is not None:
             if self.out_framework == 'torch':
-                print('passing torch')
                 y[y < np.log(self.label_prelog_cutoff_low)] = np.log(self.label_prelog_cutoff_low)
             elif self.out_framework == 'jax':
-                print('passing jax')
                 y = y.at[y < np.log(self.label_prelog_cutoff_low)].set(np.log(self.label_prelog_cutoff_low))
         
         if self.label_prelog_cutoff_high is not None:
